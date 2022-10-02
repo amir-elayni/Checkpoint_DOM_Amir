0766a7908bdf048f105cbd6f16d8405c9e930ea9
amir-elayni <medamirelayni@gmail.com>
2022-10-03 00:13:56 +0100

    added heart-shaped button

--- root.js
+++ root.js
@@ -97,3 +97,36 @@ function removeItem(event){
     grandTotal()
     
 }
+
+
+
+var btnvar1 = document.getElementById('btnh1');
+
+function Toggle1(){
+         if (btnvar1.style.color =="red") {
+             btnvar1.style.color = "grey"
+         }
+         else{
+             btnvar1.style.color = "red"
+         }
+}
+var btnvar2 = document.getElementById('btnh2');
+
+function Toggle2(){
+         if (btnvar2.style.color =="red") {
+             btnvar2.style.color = "grey"
+         }
+         else{
+             btnvar2.style.color = "red"
+         }
+}
+var btnvar3 = document.getElementById('btnh3');
+
+function Toggle3(){
+         if (btnvar3.style.color =="red") {
+             btnvar3.style.color = "grey"
+         }
+         else{
+             btnvar3.style.color = "red"
+         }
+}
